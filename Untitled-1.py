165a8bfc84e5b8bf6d9e0182fae0cfba597a1ad3
italo <n00319997@upn.pe>
2026-05-29 01:56:49 -0500

    ejercicio 1 calculadora simple y 2 area y perimetro de un circulo de lenguaje python

---
+++ Untitled-1.py
@@ -0,0 +1,11 @@
+
+
+print("Calculo del area y perimetro de un circulo")
+r=int(input("ingrese el radio del circulo: "))
+import math
+AC=math.pi*r**2
+print(f"El area del circulo es: {AC}")
+PC=2*math.pi*r
+print(f"El perimetro del circulo es: {PC}")
+    
+
---
+++ ejercicio-1.py
@@ -0,0 +1,15 @@
+
+
+
+num1=float(input("ingrese el primer número: "))
+num2=float(input("ingrese el segundo número: "))
+
+suma=num1+num2
+resta=num1-num2 
+multiplicacion=num1*num2
+division=num1/num2
+
+print(f"la suma es: {suma}")
+print(f"la resta es: {resta}")
+print(f"la multiplicación es: {multiplicacion}")
+print(f"la división es: {division}")
